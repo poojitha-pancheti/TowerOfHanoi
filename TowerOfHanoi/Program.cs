1a4d7d4f742e6832e6ab9c3378588427f3bb5d7f
Poojitha Pancheti <Poojitha.Pancheti@DESKTOP-UK1T43N>
2020-08-11 18:04:10 +0530

    Add project files.

---
+++ TowerOfHanoi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TowerOfHanoi
+{
+    class Program
+    {
+        public static void Hanoi(int n,char source,char temp,char dest)
+        {
+            if (n == 1)
+            {
+
+                Console.WriteLine("Move disk" + n + "from" + source + "-->" + dest);
+                return;
+            }
+            Hanoi(n-1,source, dest, temp);
+            Console.WriteLine("Move disk" + n + "from" + source + "-->" + temp);
+            Hanoi(n - 1, temp, source, dest);
+
+        }
+        
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Enter number of disks:");
+            n = Convert.ToInt32(Console.ReadLine());
+            Hanoi(n, 'A', 'B', 'C');
+        }
+    }
+}
